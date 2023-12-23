ce921237ae0983e4dbec0bf73d28be5d3202c697
CodeFFun <sharmasanketa899@gmail.com>
2023-12-23 19:53:06 +0545

    mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, {Application, Request, Response} from 'express'
 import dotenv from 'dotenv'
 import router from './router'
 import cors from 'cors'
+import connect from './config/db'
 
 dotenv.config()
 const app:Application = express()
@@ -10,13 +11,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
+connect
 app.use(router)
 
 app.use('/', (req:Request, res:Response) => {
     res.send('Welcome to the write app')
 })
 
-const port: string | number | null  = process.env.PORT || 8080
+const port: string | number | null  = process.env.PORT || 8000
 const server: string | number | null = process.env.HOST || 'localhost' 
 
 app.listen(port, () => {
