ef911793ce2324b95b330a2352b67e066a6551cb
CodeFFun <sharmasanketa899@gmail.com>
2024-03-01 14:31:05 +0545

    fix:user controller removed:

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -7,9 +7,6 @@ import tokenController from './tokenController'
 interface userControllerInterface {
   index(req: Request, res: Response): void
   create(req: Request, res: Response): void
-  show(req: Request, res: Response): void
-  update(req: Request, res: Response): void
-  delete(req: Request, res: Response): void
 }
 
 class userController implements userControllerInterface {
@@ -29,56 +26,6 @@ class userController implements userControllerInterface {
     } catch (err) {
       const error = errorResponse(err)
       res.json(dataResponse(error, 400, 'User validation error'))
-    }
-  }
-  async show(req: Request, res: Response) {
-    let id = req.params.id
-    try {
-      let findData = await User.findById(id)
-      res.json(dataResponse(findData, 200, ''))
-    } catch (err) {
-      //   errorResponse(err)
-    }
-  }
-  async update(req: Request, res: Response) {
-    let id = req.params.id
-    try {
-      const user = await User.findById(id)
-      await user?.updateOne(req.body)
-      return res.json(dataResponse(user, 200, 'Profile Updated'))
-    } catch (error) {
-
-      res.json(dataResponse(error, 500, 'Something went wrong'))
-    }
-  }
-  async delete(req: Request, res: Response) {
-    let id = req.params.id
-    try {
-      const user = await User.findById(id)
-      await user?.deleteOne(req.body)
-      return res.json(dataResponse(user, 200, 'Profile Deleted'))
-    } catch (err) {
-      res.json(dataResponse(err, 500, 'Something went wrong'))
-    }
-  }
-}
-
-
-
-      res.json(dataResponse(error, 500, 'Something went wrong'))
-
-    }
-  }
-  async delete(req: Request, res: Response) {
-    let id = req.params.id
-    try {
-      const user = await User.findById(id)
-      await user?.deleteOne(req.body)
-      return res.json(dataResponse(user, 200, 'Profile Deleted'))
-    } catch (err) {
-
-      res.json(dataResponse(err, 500, 'Something went wrong'))
-
     }
   }
 }
--- src/router/User.ts
+++ src/router/User.ts
@@ -5,8 +5,5 @@ const User = express.Router()
 const userInstance = new userController()
 
 User.get('/', userInstance.index)
-User.get('/:id', userInstance.show)
-User.put('/:id', userInstance.update)
-User.delete('/:id', userInstance.delete)
 
 export default User
